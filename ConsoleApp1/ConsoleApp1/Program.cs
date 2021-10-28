b33746cda446d1ff3ec9ea1f58cc5354e4180d85
CDNievas <cdnievas@hotmail.com>
2021-10-28 16:38:19 +0200

    Initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var client = new RestClient("https://localhost:44373");
+            var request = new RestRequest("/api/check", Method.POST);
+            request.AddJsonBody(new { user="admin", pwd="admin"});
+
+            var response = client.Execute(request);
+
+            var obj = JObject.Parse(response.Content);
+
+            Console.WriteLine(obj["valid"]);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/ApiController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+
+    [Route("/api")]
+    public class ApiController : Controller
+    {
+        [Route("ping")]
+        public string Ping()
+        {
+            return "Pong";
+        }
+
+        [Route("check")]
+        [HttpPost]
+        public IActionResult checkUser([FromBody] dynamic body)
+        {
+
+            dynamic json = JsonConvert.DeserializeObject<dynamic>(body.ToString());
+
+            Console.WriteLine(json.user);
+            Console.WriteLine(json.pwd);
+
+            return Ok(new { valid = true});
+
+        }
+
+    }
+}
